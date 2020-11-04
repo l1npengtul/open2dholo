9d3a771a3253c5e136ce48f3b152ef5c3b3c292c
l1npengtul <l1npengtul@protonmail.com>
2020-11-04 17:58:04 +0900

    continue work on self signal

--- src/open2dhctrl.rs
+++ src/open2dhctrl.rs
@@ -8,34 +8,38 @@ use std::convert::TryInto;
 #[derive(NativeClass)]
 #[inherit(Control)]
 pub struct Main{
-    colorrect_path : Option<ColorRect>,
-    open2dh_mainui : Option<Control>,
-    panel_node : Option<Panel>,
-    vbox_main : Option<VBoxContainer>,
+    colorrect_path : GodotString,
+    open2dh_mainui : GodotString,
+    panel_node : GodotString,
+    vbox_main : GodotString,
+    current_size : Vector2,
 }
 
 #[methods]
 impl Main {
     fn new(owner: &Control) -> Self {
         Main{
-            colorrect_path: None, //unsafe {*owner.get_node(NodePath::from_str("ColorRect")).unwrap().assume_safe().cast::<ColorRect>().unwrap() },
-            open2dh_mainui : None, //  unsafe {*owner.get_node(NodePath::from_str("Open2GHMainUINode")).unwrap().assume_safe().cast::<Control>().unwrap()},
-            panel_node:  None, //unsafe {*owner.get_node(NodePath::from_str("Open2GHMainUINode/Panel")).unwrap().assume_safe().cast::<Panel>().unwrap()},
-            vbox_main: None, // unsafe {*owner.get_node(NodePath::from_str("Open2GHMainUINode/Panel/VBoxContainer")).unwrap().assume_safe().cast::<VBoxContainer>().unwrap()},
+            colorrect_path: GodotString::from_str(""), //unsafe {*owner.get_node(NodePath::from_str("ColorRect")).unwrap().assume_safe().cast::<ColorRect>().unwrap() },
+            open2dh_mainui : GodotString::from_str(""), //  unsafe {*owner.get_node(NodePath::from_str("Open2GHMainUINode")).unwrap().assume_safe().cast::<Control>().unwrap()},
+            panel_node:  GodotString::from_str(""), //unsafe {*owner.get_node(NodePath::from_str("Open2GHMainUINode/Panel")).unwrap().assume_safe().cast::<Panel>().unwrap()},
+            vbox_main: GodotString::from_str(""), // unsafe {*owner.get_node(NodePath::from_str("Open2GHMainUINode/Panel/VBoxContainer")).unwrap().assume_safe().cast::<VBoxContainer>().unwrap()},
+            current_size: Vector2::new(0.0,0.0),
         }
     }
 
     #[export]
     fn _ready(&self, owner: TRef<Control>) {
-        let root_viewport : &Viewport  = unsafe {&*owner.get_viewport().unwrap().assume_safe()};
-        root_viewport.connect("on_resize", owner, "on_screen_resize", VariantArray::new_shared(), 0);
-        let vector : Vector2 = root_viewport.size();
-        godot_print!("{}",vector.x);
-        godot_print!("{}",vector.y);
+        //self.colorrect_path = GodotString::from_str("ColorRect");
+        //self.open2dh_mainui = GodotString::from_str("Open2GHMainUINode");
+        //self.panel_node = GodotString::from_str("Open2GHMainUINode/Panel");
+        //self.vbox_main = GodotString::from_str("Open2GHMainUINode/Panel/VBoxContainer");
+        let root_viewport : &Viewport  = unsafe {&*owner.get_parent().unwrap().assume_safe().cast::<Viewport>().unwrap()};
+        root_viewport.connect("size_changed", owner, "on_screen_resize", VariantArray::new_shared(), 0);
     }
     #[export]
-    pub fn on_screen_resize(&self, _owner: &Control){
-        godot_print!("resize");
+    pub fn on_size_change(&self, owner: &Control){
+        godot_print!("a");
     }
+
 }
 
