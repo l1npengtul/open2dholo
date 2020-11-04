0dab66c09725a78960e0d58b921da467f6be678c
l1npengtul <l1npengtul@protonmail.com>
2020-11-04 23:50:16 +0900

    make use of OS window size and not root viewport to fix scale issues

--- src/open2dhctrl.rs
+++ src/open2dhctrl.rs
@@ -1,44 +1,38 @@
 use gdnative::{
     prelude::*,
     NativeClass, methods,
-    api::{Panel, VBoxContainer, Control}
+    api::{Panel, VBoxContainer, Control, OS}
 };
 use std::convert::TryInto;
 
 #[derive(NativeClass)]
 #[inherit(Control)]
-pub struct Main{
-    colorrect_path : GodotString,
-    open2dh_mainui : GodotString,
-    panel_node : GodotString,
-    vbox_main : GodotString,
-    current_size : Vector2,
-}
+pub struct Main;
 
 #[methods]
 impl Main {
     fn new(owner: &Control) -> Self {
-        Main{
-            colorrect_path: GodotString::from_str(""), //unsafe {*owner.get_node(NodePath::from_str("ColorRect")).unwrap().assume_safe().cast::<ColorRect>().unwrap() },
-            open2dh_mainui : GodotString::from_str(""), //  unsafe {*owner.get_node(NodePath::from_str("Open2GHMainUINode")).unwrap().assume_safe().cast::<Control>().unwrap()},
-            panel_node:  GodotString::from_str(""), //unsafe {*owner.get_node(NodePath::from_str("Open2GHMainUINode/Panel")).unwrap().assume_safe().cast::<Panel>().unwrap()},
-            vbox_main: GodotString::from_str(""), // unsafe {*owner.get_node(NodePath::from_str("Open2GHMainUINode/Panel/VBoxContainer")).unwrap().assume_safe().cast::<VBoxContainer>().unwrap()},
-            current_size: Vector2::new(0.0,0.0),
-        }
+        Main
     }
 
     #[export]
     fn _ready(&self, owner: TRef<Control>) {
-        //self.colorrect_path = GodotString::from_str("ColorRect");
-        //self.open2dh_mainui = GodotString::from_str("Open2GHMainUINode");
-        //self.panel_node = GodotString::from_str("Open2GHMainUINode/Panel");
-        //self.vbox_main = GodotString::from_str("Open2GHMainUINode/Panel/VBoxContainer");
         let root_viewport : &Viewport  = unsafe {&*owner.get_parent().unwrap().assume_safe().cast::<Viewport>().unwrap()};
-        root_viewport.connect("size_changed", owner, "on_screen_resize", VariantArray::new_shared(), 0);
+        root_viewport.connect("size_changed", owner, "on_size_change", VariantArray::new_shared(), 0);
     }
     #[export]
     pub fn on_size_change(&self, owner: &Control){
-        godot_print!("a");
+        let root_viewport_size  = OS::new()
+        //let colorrect = unsafe {&*owner.get_node("ColorRect").unwrap().assume_safe().cast::<ColorRect>().unwrap()};
+        //let main_ui = unsafe {&*owner.get_node("Open2GHMainUINode").unwrap().assume_safe().cast::<Control>().unwrap()};
+        //let panel = unsafe {&*owner.get_node("Open2GHMainUINode/Panel").unwrap().assume_safe().cast::<Panel>().unwrap()};
+        let vbox = unsafe {&*owner.get_node("Open2GHMainUINode/Panel/VBoxContainer").unwrap().assume_safe().cast::<VBoxContainer>().unwrap()};
+
+        //colorrect.set_size(root_viewport_size, true);
+        //main_ui.set_size(root_viewport_size, true);
+        //panel.set_size(root_viewport_size, true);
+        vbox.set_size(root_viewport_size, true);
+        //owner.set_size(root_viewport_size, true);
     }
 
 }
