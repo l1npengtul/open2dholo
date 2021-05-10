52b01c9448f1a8efb9b7b51ca7d4a9e74b518973
l1npengtul <l1npengtul@protonmail.com>
2021-05-10 15:18:01 +0900

    add eye ratio calc

--- src/nodes/preview_viewport.rs
+++ src/nodes/preview_viewport.rs
@@ -1,6 +1,7 @@
 use crate::wtf;
+use euclid::{UnknownUnit, Vector2D};
 use gdnative::{
-    api::{Resource, Skeleton, Viewport},
+    api::{MeshInstance, Resource, Skeleton, Viewport, VisualServer},
     prelude::*,
     NativeClass,
 };
@@ -92,8 +93,12 @@ impl PreviewViewport {
                         .unwrap()
                 };
 
-                for bone_idx in 0..model_skeleton.get_bone_count()  {
-                    if model_skeleton.get_bone_name(bone_idx).to_string().contains("neck") {
+                for bone_idx in 0..model_skeleton.get_bone_count() {
+                    if model_skeleton
+                        .get_bone_name(bone_idx)
+                        .to_string()
+                        .contains("neck")
+                    {
                         self.neck_bone_id.set(bone_idx as i32);
                     }
                 }
@@ -106,7 +111,7 @@ impl PreviewViewport {
     fn on_frame_processed(
         &self,
         owner: TRef<Viewport>,
-        facebox: Variant,
+        _facebox: Variant,
         landmarks: Variant,
         angle: Variant,
     ) {
@@ -130,15 +135,86 @@ impl PreviewViewport {
                     .unwrap()
             };
 
+            // why
+            let model_mesh_inst = unsafe {
+                owner
+                    .get_node("Spatial/Skeleton/Face")
+                    .unwrap()
+                    .assume_safe()
+                    .cast::<MeshInstance>()
+                    .unwrap()
+            };
+
+            let mm = { model_mesh_inst.mesh().unwrap() };
+
+            let model_mesh = unsafe { mm.assume_safe() };
+
+            let model_mesh_rid = model_mesh.get_rid();
+
             let angle_vec3 = angle.to_vector3();
-            let current_neck_transform = model_skeleton.get_bone_custom_pose(self.neck_bone_id.get().into());
+            let landmarks_vec = {
+                let ld = landmarks.to_vector2_array();
+                let mut p2d_vec = vec![];
+                for point_idx in 0..ld.len() {
+                    let vec2 = ld.get(point_idx);
+                    p2d_vec.push(vec2);
+                }
+                p2d_vec
+            };
+
+            let current_neck_transform =
+                model_skeleton.get_bone_custom_pose(self.neck_bone_id.get().into());
             let new_neck_tranform = Transform {
                 basis: Basis::from_euler(angle_vec3),
                 origin: current_neck_transform.origin,
             };
             model_skeleton.set_bone_custom_pose(self.neck_bone_id.get().into(), new_neck_tranform);
-            
+            let (left_eye, right_eye) = calc_ear(&landmarks_vec);
             // TODO: get face transforms here
+            let visual_server = unsafe { VisualServer::godot_singleton() };
+            for variant in model_mesh.surface_get_blend_shape_arrays(0).iter() {
+                godot_print!("{:?}", variant);
+            }
         }
     }
+}
+
+// FIXME: this is ***BAD***
+#[inline]
+fn calc_ear(landmarks: &[Vector2D<f32, UnknownUnit>]) -> (f32, f32) {
+    // eye left
+    let left = single_ear(
+        *landmarks.get(37).unwrap(),
+        *landmarks.get(38).unwrap(),
+        *landmarks.get(39).unwrap(),
+        *landmarks.get(40).unwrap(),
+        *landmarks.get(41).unwrap(),
+        *landmarks.get(42).unwrap(),
+    );
+    let right = single_ear(
+        *landmarks.get(43).unwrap(),
+        *landmarks.get(44).unwrap(),
+        *landmarks.get(45).unwrap(),
+        *landmarks.get(46).unwrap(),
+        *landmarks.get(47).unwrap(),
+        *landmarks.get(48).unwrap(),
+    );
+    (left, right)
+}
+
+#[inline]
+fn single_ear(
+    p1: Vector2D<f32, UnknownUnit>,
+    p2: Vector2D<f32, UnknownUnit>,
+    p3: Vector2D<f32, UnknownUnit>,
+    p4: Vector2D<f32, UnknownUnit>,
+    p5: Vector2D<f32, UnknownUnit>,
+    p6: Vector2D<f32, UnknownUnit>,
+) -> f32 {
+    (euclid_distance(p2, p6) + euclid_distance(p3, p5)) / (2_f32 * euclid_distance(p1, p4))
+}
+
+#[inline]
+fn euclid_distance(p1: Vector2D<f32, UnknownUnit>, p2: Vector2D<f32, UnknownUnit>) -> f32 {
+    ((p1.x - p2.x).powf(2_f32) + (p1.y - p2.y).powf(2_f32)).sqrt()
 }
